be84581bff83637a0bf456e693886f1906476d92
joaquimg <joaquimdgarcia@gmail.com>
2018-01-16 23:11:04 -0200

    rollback get obj

--- src/lqoi_objective.jl
+++ src/lqoi_objective.jl
@@ -47,15 +47,7 @@ MOI.canget(m::LinQuadSolverInstance, ::MOI.ObjectiveSense) = true
 
 function MOI.get(m::LinQuadSolverInstance, ::MOI.ObjectiveFunction)
     variable_coefficients = lqs_getobj(m)
-    coefs = Float64[]
-    vars = VarInd[]
-    for i in eachindex(variable_coefficients)
-        if abs(variable_coefficients[i]) > 1e-20
-            push!(coefs, variable_coefficients[i])
-            push!(vars, m.variable_references[i])
-        end
-    end
-    MOI.ScalarAffineFunction(vars, coefs, m.objective_constant)
+    MOI.ScalarAffineFunction(m.variable_references, variable_coefficients, m.objective_constant)
 end
 # can't get quadratic objective functions
 MOI.canget(m::LinQuadSolverInstance, ::MOI.ObjectiveFunction) = !m.obj_is_quad
