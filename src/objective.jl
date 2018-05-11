3a938d1924d29148c15bbb8c89d17d23bf5e2dda
Robin Deits <robin.deits@gmail.com>
2018-05-10 23:47:25 -0400

    support setting a single-variable objective by promoting to ScalarAffineFunction (#9)
    * support setting a single-variable objective by promoting to Affine
    
    * WIP: enum for objective type including single var
    
    * add an enum for objective type and improve SingleVariable objectives

--- src/objective.jl
+++ src/objective.jl
@@ -17,7 +17,7 @@ function MOI.set!(m::LinQuadOptimizer, ::MOI.ObjectiveSense, sense::MOI.Optimiza
         # we set the objective sense to :min, and the objective to 0.0
         change_objective_sense!(m, :min)
         unsafe_set!(m, MOI.ObjectiveFunction{Linear}(), MOI.ScalarAffineFunction(VarInd[],Float64[],0.0))
-        m.obj_is_quad = false
+        m.obj_type = AffineObjective
         m.obj_sense = MOI.FeasibilitySense
     else
         error("Sense $(sense) unknown.")
@@ -36,6 +36,16 @@ function MOI.canset(m::LinQuadOptimizer, ::MOI.ObjectiveFunction{F}) where F<:MO
     return F in supported_objectives(m)
 end
 
+function MOI.set!(m::LinQuadOptimizer, ::MOI.ObjectiveFunction{MOI.SingleVariable}, var::MOI.SingleVariable)
+    if m.obj_type == QuadraticObjective
+        set_quadratic_objective!(m, Int[], Int[], Float64[])
+    end
+    m.obj_type = SingleVariableObjective
+    m.single_obj_var = var.variable
+    set_linear_objective!(m, [getcol(m, var.variable)], [1.0])
+    m.objective_constant = 0
+end
+
 function MOI.set!(m::LinQuadOptimizer, ::MOI.ObjectiveFunction{F}, objf::Linear) where F
     cobjf = MOIU.canonical(objf)
     unsafe_set!(m, MOI.ObjectiveFunction{Linear}(), cobjf)
@@ -47,19 +57,21 @@ end
 Sets a linear objective function without cannonicalizing `objective`.
 """
 function unsafe_set!(m::LinQuadOptimizer, ::MOI.ObjectiveFunction{F}, objf::Linear) where F
-    if m.obj_is_quad
+    if m.obj_type == QuadraticObjective
         # previous objective was quadratic...
-        m.obj_is_quad = false
         # zero quadratic part
         set_quadratic_objective!(m, Int[], Int[], Float64[])
     end
+    m.obj_type = AffineObjective
+    m.single_obj_var = nothing
     set_linear_objective!(m, getcol.(m, objf.variables), objf.coefficients)
     m.objective_constant = objf.constant
     nothing
 end
 
 function MOI.set!(m::LinQuadOptimizer, ::MOI.ObjectiveFunction, objf::Quad)
-    m.obj_is_quad = true
+    m.obj_type = QuadraticObjective
+    m.single_obj_var = nothing
     set_linear_objective!(m,
         getcol.(m, objf.affine_variables),
         objf.affine_coefficients
@@ -82,7 +94,12 @@ end
     Get the objective function
 =#
 
-MOI.canget(m::LinQuadOptimizer, ::MOI.ObjectiveFunction{Linear}) = !m.obj_is_quad
+MOI.canget(m::LinQuadOptimizer, ::MOI.ObjectiveFunction{MOI.SingleVariable}) = m.obj_type == SingleVariableObjective
+function MOI.get(m::LinQuadOptimizer, ::MOI.ObjectiveFunction{MOI.SingleVariable})
+    SingleVariable(get(m.single_obj_var))
+end
+
+MOI.canget(m::LinQuadOptimizer, ::MOI.ObjectiveFunction{Linear}) = m.obj_type != QuadraticObjective
 function MOI.get(m::LinQuadOptimizer, ::MOI.ObjectiveFunction{Linear})
     variable_coefficients = zeros(length(m.variable_references))
     get_linear_objective!(m, variable_coefficients)
@@ -95,6 +112,10 @@ end
 
 MOI.canmodifyobjective(m::LinQuadOptimizer, ::Type{MOI.ScalarCoefficientChange{Float64}}) = true
 function MOI.modifyobjective!(m::LinQuadOptimizer, chg::MOI.ScalarCoefficientChange{Float64})
+    if m.obj_type == SingleVariableObjective
+        m.obj_type = AffineObjective
+        m.single_obj_var = nothing
+    end
     col = m.variable_mapping[chg.variable]
     # 0 row is the objective
     change_coefficient!(m, 0, col, chg.new_coefficient)
