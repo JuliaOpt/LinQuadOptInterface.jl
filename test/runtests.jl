5bea8fa534da1cebed7ca91595230c9e0bdfd8e3
joaquimg <joaquimdgarcia@gmail.com>
2018-05-19 16:17:20 -0300

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,8 +7,38 @@ const MOIT = MathOptInterface.Test
 const LQOI = LinQuadOptInterface
 
 
-@testset "Linear tests" begin
-    linconfig = MOIT.TestConfig(solve = false)
-    solver = LQOI.MockLinQuadOptimizer()
-    MOIT.contlineartest(solver , linconfig, ["linear10"])
+@testset "LinQuadOptInterface" begin
+    @testset "Linear tests" begin
+        linconfig = MOIT.TestConfig(solve = false)
+        solver = LQOI.MockLinQuadOptimizer()
+        MOIT.contlineartest(solver , linconfig, ["linear10"])
+    end
+
+    # @testset "Quadratic tests" begin
+    #     quadconfig = MOIT.TestConfig(solve=false)
+    #     solver = LQOI.MockLinQuadOptimizer()
+    #     MOIT.contquadratictest(solver, quadconfig)
+    # end
+
+    @testset "Linear Conic tests" begin
+        linconfig = MOIT.TestConfig(solve=false)
+        solver = LQOI.MockLinQuadOptimizer()
+        MOIT.lintest(solver, linconfig)
+    end
+
+    @testset "Integer Linear tests" begin
+        intconfig = MOIT.TestConfig(solve=false)
+        solver = LQOI.MockLinQuadOptimizer()
+        MOIT.intlineartest(solver, intconfig, ["int2"])
+    end
+
+    @testset "ModelLike tests" begin
+        intconfig = MOIT.TestConfig(solve=false)
+        solver = LQOI.MockLinQuadOptimizer()
+        MOIT.validtest(solver)
+        MOIT.emptytest(solver)
+        solver2 = LQOI.MockLinQuadOptimizer()
+        MOIT.copytest(solver,solver2)
+    end
 end
+;
