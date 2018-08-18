b9888adba7a4af3b07a476b289c2211ad2470940
Robin Deits <robin.deits@gmail.com>
2018-08-17 22:28:14 -0400

    add a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -55,3 +55,16 @@ const LQOI = LinQuadOptInterface
         end
     end
 end
+
+@testset "Issue #52" begin
+    model = LQOI.MockLinQuadOptimizer()
+    x = MOI.addvariable!(model)
+    f1 =  MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([1.0], [x]), 1.5)
+    s1 = MOI.LessThan(2.0)
+    c = MOI.addconstraint!(model, f1, s1)
+    f2 = MOI.get(model, MOI.ConstraintFunction(), c)
+    @test f1 ≈ f2
+    s2 = MOI.get(model, MOI.ConstraintSet(), c)
+    @test typeof(s1) == typeof(s2)
+    @test s1.upper == s2.upper
+end
