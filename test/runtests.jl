7536c3e01e651ae3a02504a7420f58e54d8e5f07
Robin Deits <robin.deits@gmail.com>
2018-08-19 12:03:23 -0400

    clean up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -118,6 +118,21 @@ end
 end
 
 @testset "Issue #32" begin
+    check_set(actual::MOI.GreaterThan, expected::MOI.GreaterThan) =
+        @test actual.lower == expected.lower
+    check_set(actual::MOI.LessThan, expected::MOI.LessThan) =
+        @test actual.upper == expected.upper
+    function check_set(actual::MOI.Interval, expected::MOI.Interval)
+        @test actual.lower == expected.lower
+        @test actual.lower == expected.lower
+    end
+    function check_row(model, constraint_index, expected_function, expected_set)
+        actual_function = MOI.get(model, MOI.ConstraintFunction(), constraint_index)
+        @test actual_function ≈ expected_function
+        actual_set = MOI.get(model, MOI.ConstraintSet(), constraint_index)
+        check_set(actual_set, expected_set)
+    end
+
     @testset "Simple scalar affine function replacement" begin
         model = LQOI.MockLinQuadOptimizer()
         x = MOI.addvariable!(model)
@@ -126,46 +141,30 @@ end
         f =  MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([2.0], [x]), 0.1)
         s = MOI.GreaterThan(0.5)
         c = MOI.addconstraint!(model, f, s)
-        @test MOI.get(model, MOI.ConstraintFunction(), c) ≈ f
-        s2 = MOI.get(model, MOI.ConstraintSet(), c)
-        @test typeof(s2) == typeof(s)
-        @test s2.lower == s.lower
+        check_row(model, c, f, s)
 
         # Replace the constraint function with itself and verify that
         # the problem is unchanged
         MOI.set!(model, MOI.ConstraintFunction(), c, f)
-        @test MOI.get(model, MOI.ConstraintFunction(), c) ≈ f
-        s2 = MOI.get(model, MOI.ConstraintSet(), c)
-        @test typeof(s2) == typeof(s)
-        @test s2.lower == s.lower
+        check_row(model, c, f, s)
 
         # Replace the constraint function with a new function, verify
         # that the replacement occurred and that the set is unchanged
         f2 = MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([1.0], [x]), 0.5)
         MOI.set!(model, MOI.ConstraintFunction(), c, f2)
-        @test MOI.get(model, MOI.ConstraintFunction(), c) ≈ f2
-        s2 = MOI.get(model, MOI.ConstraintSet(), c)
-        @test typeof(s2) == typeof(s)
-        @test s2.lower == s.lower
+        check_row(model, c, f2, s)
 
         # Replace the constraint function with a new function which is not
         # in canonical form:
         f3 = MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([1.0, 0.5], [x]), 0.5)
         MOI.set!(model, MOI.ConstraintFunction(), c, f3)
-        @test MOI.get(model, MOI.ConstraintFunction(), c) ≈ MOIU.canonical(f3)
-        s2 = MOI.get(model, MOI.ConstraintSet(), c)
-        @test typeof(s2) == typeof(s)
-        @test s2.lower == s.lower
-
+        check_row(model, c, MOIU.canonical(f3), s)
 
         # Replace the constraint function with a new function whose sparsity
         # pattern is different:
         f4 = MOI.ScalarAffineFunction(MOI.ScalarAffineTerm{Float64}[], 2.0)
         MOI.set!(model, MOI.ConstraintFunction(), c, f4)
-        @test MOI.get(model, MOI.ConstraintFunction(), c) ≈ f4
-        s2 = MOI.get(model, MOI.ConstraintSet(), c)
-        @test typeof(s2) == typeof(s)
-        @test s2.lower == s.lower
+        check_row(model, c, f4, s)
     end
 end
 
