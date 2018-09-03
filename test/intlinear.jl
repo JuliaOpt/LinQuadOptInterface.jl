e627023fcd04699d79840c17b3428c03c1f935bd
joaquimgarcia_psr <joaquimgarcia@psr-inc.com>
2018-09-03 11:51:47 -0300

    test new set solution in intlinear

--- test/intlinear.jl
+++ test/intlinear.jl
@@ -1,62 +1,70 @@
 function set_knapsacktest_solutions!(solver)
     # SOLVE 1
-    LQOI.set_termination_status!(solver, MOI.Success)
-    LQOI.set_primal_status!(solver, MOI.FeasiblePoint)
-    LQOI.set_dual_status!(solver, MOI.UnknownResultStatus)
+    LQOI.set_solution!(solver,
+    termination_status = MOI.Success,
+    primal_status = MOI.FeasiblePoint,
+    dual_status = MOI.UnknownResultStatus,
 
-    LQOI.set_variable_primal_solution!(solver, [1.0, 0.0, 0.0, 1.0, 1.0])
-    LQOI.set_constraint_primal_solution!(solver, [9.0])
-    LQOI.set_quadratic_primal_solution!(solver, Float64[])
+    variable_primal = [1.0, 0.0, 0.0, 1.0, 1.0],
+    constraint_primal = [9.0],
+    quadratic_primal = Float64[],
 
-    LQOI.set_variable_dual_solution!(solver, [NaN, NaN, NaN, NaN, NaN])
-    LQOI.set_constraint_dual_solution!(solver, [NaN])
-    LQOI.set_quadratic_dual_solution!(solver, Float64[])
+    variable_dual = [NaN, NaN, NaN, NaN, NaN],
+    constraint_dual = [NaN],
+    quadratic_dual = Float64[]
+    )
 
     nothing
 end
 
 function set_int1test_solutions!(solver)
     # SOLVE 1
-    LQOI.set_termination_status!(solver, MOI.Success)
-    LQOI.set_primal_status!(solver, MOI.FeasiblePoint)
-    LQOI.set_dual_status!(solver, MOI.UnknownResultStatus)
+    LQOI.set_solution!(solver,
+    termination_status = MOI.Success,
+    primal_status = MOI.FeasiblePoint,
+    dual_status = MOI.UnknownResultStatus,
 
-    LQOI.set_variable_primal_solution!(solver, [4.0, 5.0, 1.0])
-    LQOI.set_constraint_primal_solution!(solver, [10.0, 15.0])
-    LQOI.set_quadratic_primal_solution!(solver, Float64[])
+    variable_primal = [4.0, 5.0, 1.0],
+    constraint_primal = [10.0, 15.0],
+    quadratic_primal = Float64[],
 
-    LQOI.set_variable_dual_solution!(solver, [NaN, NaN, NaN])
-    LQOI.set_constraint_dual_solution!(solver, [NaN, NaN])
-    LQOI.set_quadratic_dual_solution!(solver, Float64[])
+    variable_dual = [NaN, NaN, NaN],
+    constraint_dual = [NaN, NaN],
+    quadratic_dual = Float64[]
+    )
 
     nothing
 end
 
 function set_int3test_solutions!(solver)
     # SOLVE 1
-    LQOI.set_termination_status!(solver, MOI.Success)
-    LQOI.set_primal_status!(solver, MOI.FeasiblePoint)
-    LQOI.set_dual_status!(solver, MOI.UnknownResultStatus)
+    LQOI.set_solution!(solver,
+    termination_status = MOI.Success,
+    primal_status = MOI.FeasiblePoint,
+    dual_status = MOI.UnknownResultStatus,
 
-    LQOI.set_variable_primal_solution!(solver, [1.0, -0.0, -0.0, -0.0, -0.0, 1.0, -0.0, -0.0, -0.0, -0.0, -0.0])
-    LQOI.set_constraint_primal_solution!(solver, [0.9875])
-    LQOI.set_quadratic_primal_solution!(solver, Float64[])
+    variable_primal = [1.0, -0.0, -0.0, -0.0, -0.0, 1.0, -0.0, -0.0, -0.0, -0.0, -0.0],
+    constraint_primal = [0.9875],
+    quadratic_primal = Float64[],
 
-    LQOI.set_variable_dual_solution!(solver, [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN])
-    LQOI.set_constraint_dual_solution!(solver, [NaN])
-    LQOI.set_quadratic_dual_solution!(solver, Float64[])
+    variable_dual = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN],
+    constraint_dual = [NaN],
+    quadratic_dual = Float64[]
+    )
 
     # SOLVE 2
-    LQOI.set_termination_status!(solver, MOI.Success)
-    LQOI.set_primal_status!(solver, MOI.FeasiblePoint)
-    LQOI.set_dual_status!(solver, MOI.UnknownResultStatus)
+    LQOI.set_solution!(solver,
+    termination_status = MOI.Success,
+    primal_status = MOI.FeasiblePoint,
+    dual_status = MOI.UnknownResultStatus,
 
-    LQOI.set_variable_primal_solution!(solver, [1.0, -0.0, -0.0, -0.0, -0.0, 1.0, -0.0, -0.0, -0.0, -0.0, -0.0])
-    LQOI.set_constraint_primal_solution!(solver, [0.9875])
-    LQOI.set_quadratic_primal_solution!(solver, Float64[])
+    variable_primal = [1.0, -0.0, -0.0, -0.0, -0.0, 1.0, -0.0, -0.0, -0.0, -0.0, -0.0],
+    constraint_primal = [0.9875],
+    quadratic_primal = Float64[],
 
-    LQOI.set_variable_dual_solution!(solver, [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN])
-    LQOI.set_constraint_dual_solution!(solver, [NaN])
-    LQOI.set_quadratic_dual_solution!(solver, Float64[])
+    variable_dual = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN],
+    constraint_dual = [NaN],
+    quadratic_dual = Float64[],
+    )
     nothing
 end
