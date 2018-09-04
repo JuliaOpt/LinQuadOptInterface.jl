5679b061e7e7656a7fe11b121bdbc97277e1550d
joaquimgarcia_psr <joaquimgarcia@psr-inc.com>
2018-09-04 13:40:27 -0300

    remove more default statuses

--- test/contquadratic.jl
+++ test/contquadratic.jl
@@ -1,9 +1,6 @@
 function set_qp1test_solutions!(solver)
     # SOLVE 1
     LQOI.set_solution!(solver,
-        termination_status = MOI.Success,
-        primal_status = MOI.FeasiblePoint,
-        dual_status = MOI.FeasiblePoint,
         variable_primal = [0.571429, 0.428572, 0.857143],
         constraint_primal = [4.0, 1.0],
         variable_dual = [0.0, 0.0, 0.0],
@@ -15,9 +12,6 @@ end
 function set_qp2test_solutions!(solver)
     # SOLVE 1
     LQOI.set_solution!(solver,
-        termination_status = MOI.Success,
-        primal_status = MOI.FeasiblePoint,
-        dual_status = MOI.FeasiblePoint,
         variable_primal = [0.571429, 0.428572, 0.857143],
         constraint_primal = [4.0, 1.0],
         variable_dual = [0.0, 0.0, 0.0],
@@ -25,9 +19,6 @@ function set_qp2test_solutions!(solver)
     )
     # SOLVE 2
     LQOI.set_solution!(solver,
-        termination_status = MOI.Success,
-        primal_status = MOI.FeasiblePoint,
-        dual_status = MOI.FeasiblePoint,
         variable_primal = [0.571429, 0.428572, 0.857143],
         constraint_primal = [4.0, 1.0],
         variable_dual = [-0.0, -0.0, -0.0],
@@ -39,9 +30,6 @@ end
 function set_qp3test_solutions!(solver)
     # SOLVE 1
     LQOI.set_solution!(solver,
-        termination_status = MOI.Success,
-        primal_status = MOI.FeasiblePoint,
-        dual_status = MOI.FeasiblePoint,
         variable_primal = [0.250002, 0.749998],
         constraint_primal = [1.0],
         variable_dual = [6.97141e-6, 4.06505e-8],
@@ -49,9 +37,6 @@ function set_qp3test_solutions!(solver)
     )
     # SOLVE 2
     LQOI.set_solution!(solver,
-        termination_status = MOI.Success,
-        primal_status = MOI.FeasiblePoint,
-        dual_status = MOI.FeasiblePoint,
         variable_primal = [1.0, 0.0],
         constraint_primal = [1.0],
         variable_dual = [-0.0, -1.0],
@@ -63,9 +48,6 @@ end
 function set_qcp1test_solutions!(solver)
     # SOLVE 1
     LQOI.set_solution!(solver,
-        termination_status = MOI.Success,
-        primal_status = MOI.FeasiblePoint,
-        dual_status = MOI.FeasiblePoint,
         variable_primal = [0.500003, 1.75],
         constraint_primal = [1.25, 2.25],
         quadratic_primal = [2.0],
@@ -79,9 +61,6 @@ end
 function set_qcp2test_solutions!(solver)
     # SOLVE 1
     LQOI.set_solution!(solver,
-        termination_status = MOI.Success,
-        primal_status = MOI.FeasiblePoint,
-        dual_status = MOI.FeasiblePoint,
         variable_primal = [1.41421],
         constraint_primal = Float64[],
         quadratic_primal = [2.0],
@@ -95,9 +74,6 @@ end
 function set_qcp3test_solutions!(solver)
     # SOLVE 1
     LQOI.set_solution!(solver,
-        termination_status = MOI.Success,
-        primal_status = MOI.FeasiblePoint,
-        dual_status = MOI.FeasiblePoint,
         variable_primal = [1.41421],
         constraint_primal = Float64[],
         quadratic_primal = [2.0],
@@ -111,9 +87,6 @@ end
 function set_socp1test_solutions!(solver)
     # SOLVE 1
     LQOI.set_solution!(solver,
-        termination_status = MOI.Success,
-        primal_status = MOI.FeasiblePoint,
-        dual_status = MOI.FeasiblePoint,
         variable_primal = [0.5, 0.5, 0.707107],
         constraint_primal = [1.0],
         quadratic_primal = [-9.55708e-13],
